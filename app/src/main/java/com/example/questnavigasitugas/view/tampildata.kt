363384664c8607e3837982eff14f892ae71aba9a
riqsans <thrq88@gmail.com>
2025-11-07 20:10:32 +0700

    menambahkan tampilan data dan tombol navigasi

--- app/src/main/java/com/example/questnavigasitugas/view/tampildata.kt
+++ app/src/main/java/com/example/questnavigasitugas/view/tampildata.kt
@@ -48,6 +48,34 @@ fun TampilData(
                         (id = R.color.teal_700)
                     )
             )
-        })
+        }){ isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onToFormBtnClick) {
+                    Text(text = stringResource(id=R.string.FormulirPendaftaran))
+                }
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onToHomeBtnClick) {
+                    Text(text = stringResource(id=R.string.home))
+                }
+            }
+        }
+    }
 
 }
