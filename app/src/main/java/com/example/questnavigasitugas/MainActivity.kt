d4c44fdfc6f2401a819c12041b2120150fd20c65
riqsans <thrq88@gmail.com>
2025-11-07 20:12:20 +0700

    mengganti Greeting dengan DataApp

--- app/src/main/java/com/example/questnavigasitugas/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasitugas/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestNavigasiTugasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
