0617c670a9c8483a3b9e33f666c90d9c74332872
riqsans <thrq88@gmail.com>
2025-11-07 19:15:24 +0700

    membuat navigasi untuk aplikasi

--- app/src/main/java/com/example/questnavigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasitugas/Navigasi.kt
@@ -1,2 +1,14 @@
 package com.example.questnavigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas.view.FormulirPendaftaran
+import com.example.questnavigasitugas.view.TampilData
+import com.example.questnavigasitugas.view.home
+
